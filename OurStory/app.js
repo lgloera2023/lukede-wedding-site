cebc21e036652584ae50f30c55fe997f7d063cec
Luke Loera <lgloera2023@gmail.com>
2021-02-21 14:21:02 -0600

    1.1.07

--- OurStory/app.js
+++ OurStory/app.js
@@ -2,9 +2,12 @@
  * Created By: Luke Loera
  * Date: 12/30/2020
  * Description:
- * Provides the animations for the story slideshows.
+ * Provides the initial alert to give instructions on how to view the
+ * stories and also provides the animations for the story slideshows.
  *************************************************************************/
 
+// Initial alert
+setTimeout(function() {alert("(HOW TO LOOK AT STORIES)\nScroll through the stories by clicking on the box outlines.");}, 250);
 
 //////////////////////////////////////////////////////////////////////////
 // Pulse white
